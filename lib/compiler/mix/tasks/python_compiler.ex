48070c8e195b8931255e96d71ff37735d251fcec
OdielDomanie <meozkara+git@gmail.com>
2023-03-08 15:32:48 +0300

    Check existing installation

--- lib/compiler/mix/tasks/python_compiler.ex
+++ lib/compiler/mix/tasks/python_compiler.ex
@@ -1,26 +1,39 @@
 defmodule Mix.Tasks.Compile.Python do
   @moduledoc """
   Create a Python venv and install dependencies via pip.
-  pip depencies are declared in `python_depencies/0` in `mix.exs`.
+  `mix.exs` should have two more keys
+  `:python_bin` and `:pip_deps`.
   """
 
   use Mix.Task.Compiler
 
   @venv_dir Path.join(Mix.Project.app_path(), "venv")
+  @python Path.join(@venv_dir, "bin/python")
+  @manifest Path.join(@venv_dir, "python_from")
 
   # Compiler must return {:ok | :noop | :error, [diagnostic]}
-  # TODO: return better
   def run(_args) do
+    python_bin = Keyword.fetch!(Mix.Project.config(), :python_bin)
+
+    if File.exists?(@python) || ({:ok, ^python_bin} = File.read(@manifest)) do
+      {:noop, []}
+    else
+      do_compile()
+    end
+  end
+
+  defp do_compile() do
     config = Mix.Project.config()
+
+    _ = clean()
 
     with {_, 0} <-
            config
            |> Keyword.fetch!(:python_bin)
            |> System.cmd(["-m", "venv", @venv_dir], stderr_to_stdout: true),
-         python = Path.join(@venv_dir, "bin/python"),
          {_, 0} <-
            System.cmd(
-             python,
+             @python,
              ["-m", "pip", "install", "-y", "-U" | Keyword.fetch!(config, :pip_deps)],
              stderr_to_stdout: true
            ) do
@@ -32,7 +45,7 @@ defmodule Mix.Tasks.Compile.Python do
            %Mix.Task.Compiler.Diagnostic{
              compiler_name: "Python",
              details: ret,
-             file: __ENV__.file,
+             file: Mix.Project.project_file(),
              message: inspect(out),
              position: 0,
              severity: :error
@@ -41,7 +54,9 @@ defmodule Mix.Tasks.Compile.Python do
     end
   end
 
-  def clean do
+  def manifests, do: [@manifest]
+
+  def clean() do
     File.rmdir(@venv_dir)
   end
 end
